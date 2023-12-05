2ac9ae0425a315b1da71dba92cb4098ad68437bc
Serhii Babanov <serhii.babanov@gmail.com>
2023-12-04 19:58:35 -0500

    add HAL in SimilarProductsController response

--- src/main/java/com/candles/demo/DemoApplication.java
+++ src/main/java/com/candles/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.candles.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
